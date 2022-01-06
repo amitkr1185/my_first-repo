44dd62d519cd5bfea79409af9017ced9e118732c
amitkr1185 <97225686+amitkr1185@users.noreply.github.com>
2022-01-07 00:54:13 +0530

    Create data_source.tf

---
+++ data_source.tf
@@ -0,0 +1,11 @@
+data "aws_ami" "data_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
